170b5adad2e15479b6c23ba95eea165a0bcc129f
ctih1 <onni09nevala@gmail.com>
2025-11-21 18:01:59 +0200

    added new game

--- src/routes/api/traceroute/+server.ts
+++ src/routes/api/traceroute/+server.ts
@@ -16,35 +16,34 @@ const KNOWN_REPLACEMENTS = new Map(Object.entries({
     "kbn": "cph",
     "kan": "mci",
     "pal": "pao",
-    "chi": "ord"
+    "chi": "ord",
+    "prs": "cdg"
 }));
 
-const TEST_TRACEROUTE = `traceroute to 104.248.99.119 (104.248.99.119), 25 hops max, 60 byte packets
- 1  DESKTOP-K8CPH23.mshome.net (172.26.160.1)  1.415 ms
- 2  192.168.32.1 (192.168.32.1)  1.360 ms
- 3  *
- 4  *
- 5  10.209.5.37 (10.209.5.37)  59.754 ms
- 6  10.209.5.38 (10.209.5.38)  59.896 ms
- 7  *
- 8  *
- 9  hls-b4-link.ip.twelve99.net (62.115.153.140)  59.623 ms
-10  sto-bb2-link.ip.twelve99.net (62.115.123.202)  59.621 ms
-11  kbn-bb6-link.ip.twelve99.net (62.115.139.173)  60.511 ms
-12  ewr-bb2-link.ip.twelve99.net (80.91.254.91)  115.956 ms
-13  chi-bb2-link.ip.twelve99.net (62.115.132.135)  176.155 ms
-14  kanc-bb2-link.ip.twelve99.net (62.115.136.103)  174.319 ms
-15  den-bb2-link.ip.twelve99.net (62.115.140.185)  167.263 ms
-16  palo-bb2-link.ip.twelve99.net (62.115.139.112)  176.308 ms
-17  palo-b24-link.ip.twelve99.net (62.115.139.111)  162.729 ms
-18  singaporetelco-ic-335366.ip.twelve99-cust.net (62.115.8.201)  176.068 ms
-19  203.208.172.233 (203.208.172.233)  160.174 ms
-20  203.208.172.225 (203.208.172.225)  366.545 ms
-21  203.208.151.37 (203.208.151.37)  366.486 ms
-22  203.208.151.50 (203.208.151.50)  366.254 ms
-23  203.208.149.2 (203.208.149.2)  366.556 ms
-24  203.208.186.174 (203.208.186.174)  366.542 ms
-25  *`
+const TEST_TRACEROUTE = `traceroute to koti.frii.site (193.208.10.244), 25 hops max, 60 byte packets
+ 1  *
+ 2  10.74.196.63 (10.74.196.63)  1.250 ms
+ 3  138.197.248.228 (138.197.248.228)  1.285 ms
+ 4  143.244.192.168 (143.244.192.168)  0.970 ms
+ 5  143.244.225.90 (143.244.225.90)  1.935 ms
+ 6  143.244.225.23 (143.244.225.23)  1.202 ms
+ 7  ix-ae-22-0.tcore4.njy-newark.as6453.net (66.198.111.6)  1.649 ms
+ 8  if-ae-1-3.tcore4.njy-newark.as6453.net (216.6.57.6)  2.756 ms
+ 9  63.243.218.8 (63.243.218.8)  2.168 ms
+10  *
+11  *
+12  kbn-bb6-link.ip.twelve99.net (80.91.254.90)  84.885 ms
+13  prs-bb2-link.ip.twelve99.net (62.115.140.106)  118.245 ms
+14  adm-bb2-link.ip.twelve99.net (62.115.137.4)  116.014 ms
+15  sto-bb1-link.ip.twelve99.net (62.115.139.180)  95.215 ms
+16  *
+17  *
+18  *
+19  telia-ic-366423.ip.twelve99-cust.net (62.115.153.141)  118.735 ms
+20  *
+21  141.208.192.78 (141.208.192.78)  118.424 ms
+22  *
+23  mobile-access-c1d00a-244.dhcp.inet.fi (193.208.10.244)  124.233 ms`
 
 const database = new Database('ips.db');
 
@@ -85,7 +84,7 @@ async function parseOutput(lines: string[]): Promise<ProbeResult[]> {
         
         for(let city of rawCityData) {
             if(domain.toLowerCase().includes(city.city.toLocaleLowerCase()) && city.city.length > 5) {
-                if(Number(city.population) > 5000 && Number(city.population) > (result.domainAnalysis?.population || 0)) {
+                if(Number(city.population) > 50000 && Number(city.population) > (result.domainAnalysis?.population || 0)) {
                     console.log("Matched city " + city.city);
                     result.domainAnalysis = {
                         cityOrAirport: city.city.toLocaleLowerCase(),
@@ -172,12 +171,18 @@ async function getLocationFromIp(ip: string): Promise<[number, number]> {
 }
 
 export async function GET({ request }) {
-    const ip = request.headers.get("X-Real-IP");
-    if(!net.isIP(ip!)) {
-        console.log("Invalid ip! Headers: ");
-        console.log(request.headers);
-        return error(422);
-    }
+    // const ip = request.headers.get("X-Real-IP");
+    // if(!net.isIP(ip!)) {
+    //     console.log("Invalid ip! Headers: ");
+    //     console.log(request.headers);
+    //     return error(422);
+    // }
+
+    return new Response(JSON.stringify(await parseOutput(TEST_TRACEROUTE.split("\n"))), {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 
 
     return new Promise(async (resolve) => {
---
+++ src/routes/games/phone-charging-sim/+page.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+
+    let percentage = $state(0);
+    let charging = $state(false);
+    let turnedOn = $state(false);
+    let bricked = $state(false);
+    let wattage = $state(2);
+
+    let maxWattageForDevice = $state((Math.random()*95)+5);
+
+    let cable: HTMLDivElement | undefined = $state();
+    let chargingPort: HTMLDivElement | undefined = $state();
+
+    let dragging = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+    type MoveEvent = MouseEvent & { currentTarget: EventTarget & HTMLElement; }
+
+    function cableMoveStart(event: MoveEvent) {
+        if(!cable) return
+        
+
+        offsetX = event.clientX - cable.offsetLeft;
+        offsetY = event.clientY - cable.offsetTop;
+        dragging = true;
+        document.body.style.userSelect = "none";
+    }
+
+    function cableMoveEnd() {
+        dragging = false;
+        document.body.style.userSelect = ""
+    }
+
+    if(browser) {
+        document.addEventListener("mousemove", event => {
+            if(!dragging || !cable || !chargingPort) return;
+
+            let topPos = event.clientY - offsetY
+
+            if(topPos < document.body.clientHeight - cable.clientHeight) {
+                topPos = document.body.clientHeight - cable.clientHeight
+            }
+
+            let leftPos = event.clientX - offsetX;
+
+            let chargingRect = chargingPort.getBoundingClientRect();
+            let cableRect = cable.getBoundingClientRect();
+        
+            cable.style.left = leftPos + "px"
+            cable.style.top = topPos + "px";
+
+            
+            if(
+                Math.abs(chargingRect.left - cableRect.left) < 20 && Math.abs(chargingRect.right - cableRect.right) < 20 &&
+                Math.abs(chargingRect.top - cableRect.top) < 20
+            ) {
+                if(!charging) {
+                    turnedOn = true;
+                }
+                charging = true;
+            } else {
+                charging = false
+            }
+        });
+    }
+
+    setInterval(() => {
+        if(Math.round((Math.round(Math.random()*10)/10)*(Math.max(0,95-wattage))) == 0 && wattage > maxWattageForDevice) {
+            bricked = true;
+        }
+
+        if(charging) {
+            percentage += wattage/100;
+        } else if(percentage > 0) {
+            percentage -= 0.005;
+        }
+    }, 1000);
+</script>
+
+<h1>Phone charging sim</h1>
+
+<div class="wattage w-96">
+    <p>Charger wattage: {wattage}w</p>
+    <input class="w-96" type="range" bind:value={wattage} min=0 max=100>
+    <div class="flex justify-between">
+        <p>0w</p>
+        <p>100w</p>
+    </div>
+
+    <p>note: ramping up the wattage may cause the device to explode<br> <i>Explodes at a level 6 with deeper combustion at a level 7</i></p>
+</div>
+
+<div id="phone" class="w-80">
+    <div class="flex items-center">
+        <div class="aspect-9/20 rounded-2xl w-80 bg-black flex justify-center items-center">
+            {#if bricked}
+                <div class="flex flex-col justify-center">
+                    <h1 class="text-center">your device exploded <br> :(</h1>
+                    <button class="ml-auto mr-auto" onclick={_ => location.reload()}>restart</button>
+                </div>
+            {:else if turnedOn}
+                <h1>{Math.round(percentage*100)/100}%</h1>
+            {/if}
+        </div>
+        <button onclick={_ => turnedOn = !turnedOn} class="bg-zinc-900 h-32 w-2 active:bg-white">I</button>
+    </div>
+
+    <div bind:this={chargingPort} id="charging-port" class="w-8 h-2 bg-white ml-auto mr-auto"></div>
+</div>
+
+
+
+<div bind:this={cable} onmouseup={cableMoveEnd} onmousedown={cableMoveStart} id="cable" class="w-6 absolute -z-10">
+    <div id="top" class="bg-amber-300 w-4 h-4 ml-auto mr-auto rounded-t-md"></div>
+    <div id="bottom" class="bg-white w-6 h-96 rounded-t-md"></div>
+</div>
+
+<div class="spacer h-40 w-1"></div>
