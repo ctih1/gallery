6f9fbd6d1e4cacfbb338c1cfd4f61f548dcdfde1
ctih1 <onni09nevala@gmail.com>
2026-01-25 13:27:11 +0200

    added devices description

--- src/lib/components/ClearContainer.svelte
+++ src/lib/components/ClearContainer.svelte
@@ -12,7 +12,7 @@
 
 <ClearBase
     className={twMerge(
-        `md:w-9/12 lg:w-8/12 w-11/12 ml-auto -z-10 mr-auto p-4 mt-4 max-w-2xl ${!clear ? "bg-black/30" : ""}`,
+        `md:w-9/12 lg:w-8/12 w-11/12 ml-auto -z-10 mr-auto p-4 mt-4 max-w-4xl ${!clear ? "bg-black/30" : ""}`,
         className
     )}
 >
