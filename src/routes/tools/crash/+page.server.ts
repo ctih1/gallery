4366b8573270aff2167519d9d19939bad4bc57dd
ctih1 <onni09nevala@gmail.com>
2026-07-29 15:12:59 +0300

    made prerender falsde in crash

--- src/routes/tools/crash/+page.server.ts
+++ src/routes/tools/crash/+page.server.ts
@@ -1,6 +1,8 @@
 import { building } from "$app/environment";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+export const prerender = false;
+
 export const load: PageServerLoad = async ({ params }) => {
     if (!building) {
         throw error(500, "Internal server error");
