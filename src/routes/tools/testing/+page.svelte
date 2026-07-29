3b95463699b983c8b3799b279668b56bdc3cc937
ctih1 <onni09nevala@gmail.com>
2026-07-29 15:07:30 +0300

    remove dead link

--- src/routes/tools/testing/+page.svelte
+++ src/routes/tools/testing/+page.svelte
@@ -58,4 +58,3 @@
 <Input bind:value={color2percentage} label="Colour 2s starting %" type="range" />
 
 <div bind:this={testElement} class="aspect-square w-64"></div>
-<a href="/tools/crash/ignore">Crash page</a>
